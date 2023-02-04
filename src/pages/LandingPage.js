27439fcf58648ff229ac08545a839b7e4e20d5eb
Abhay <abhaychoudhary804@gmail.com>
2023-02-03 21:20:51 -0500

    fixed sidebar issue

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -13,23 +13,23 @@ export default function LandingPage() {
                 <hr />
                 <div className="sidebar--content">
                     <a href="delete">
-                        <img src="images/delete.png" width="20px" style={{marginRight:"10px"}}/><span href="#clear_conversations">Clear conversations</span>
+                        <img src="images/delete.png" width="20px" style={{ marginRight: "10px" }} /><span href="#clear_conversations">Clear conversations</span>
                     </a>
                 </div>
 
                 <div className="sidebar--content">
                     <a href="darkmode">
-                        <img src="images/moon.png" width="20px" style={{marginRight:"10px"}} /><span href="#darkmode">Dark Mode</span>
+                        <img src="images/moon.png" width="20px" style={{ marginRight: "10px" }} /><span href="#darkmode">Dark Mode</span>
                     </a>
                 </div>
                 <div className="sidebar--content">
                     <a href="#contact">
-                        <img src="images/contact.png" width="20px" style={{marginRight:"10px"}}/><span href="#contact_developers">Contact Developers</span>
+                        <img src="images/contact.png" width="20px" style={{ marginRight: "10px" }} /><span href="#contact_developers">Contact Developers</span>
                     </a>
                 </div>
                 <div className="sidebar--content">
                     <a href="#logout">
-                        <img src="images/logout.png" width="20px" style={{marginRight:"10px"}}/><span href="#log_out">Log Out</span>
+                        <img src="images/logout.png" width="20px" style={{ marginRight: "10px" }} /><span href="#log_out">Log Out</span>
                     </a>
                 </div>
 
