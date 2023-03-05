bf22139f31fd58b7ba17f4647300d5b55059eb3d
Shantanu2k19 <shantanusingh2030@gmail.com>
2023-03-05 14:31:24 +0530

    Nextjs front page added, need to integrate into project

---
+++ front-page/app/page.jsx
@@ -0,0 +1,18 @@
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import Testimonials from "@/components/Testimonials";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <AboutSectionTwo />
+      <Testimonials />
+    </>
+  );
+}
